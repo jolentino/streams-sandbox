04ab4dcd1854b567bdc87b42d104115ead53c828
jolentino <johnm.tolentino@gmail.com>
2020-12-23 10:04:35 -0500

    Installed redux-thunk and implemented dev tools

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import reducers from './reducers';
 import App from './components/App';
 
+// Redux Dev Tool
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 ReactDOM.render(
-	<Provider store={createStore(reducers)}>
+	<Provider store={createStore(reducers, composeEnhancers(applyMiddleware(thunk)))}>
 		<App />
 	</Provider>,
 	document.getElementById('root')
