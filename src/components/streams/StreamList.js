3e6b4f9dc117ac7ad81a0cb5718875baed142d35
jolentino <johnm.tolentino@gmail.com>
2020-12-27 13:23:15 -0500

    StreamList render

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-const StreamList = () => {
-	return <div>StreamList</div>;
+class StreamList extends React.Component {
+	componentDidMount() {
+		this.props.fetchStreams();
+	}
+
+	renderStreams = () => {
+		if (this.props.streams.length > 0) {
+			return this.props.streams.map(({ id, title, description }) => {
+				return (
+					<div key={id} className="item">
+						<i className="large middle aligned icon camera" />
+						<div className="content">
+							{title}
+							<div className="description">{description}</div>
+						</div>
+					</div>
+				);
+			});
+		}
+	};
+
+	render() {
+		return (
+			<div>
+				<h2>Streams</h2>
+				<div className="ui celled list">{this.renderStreams()}</div>
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = (state) => {
+	// 'Object.values' creates an array of our streams.
+	return { streams: Object.values(state.streams) };
 };
 
-export default StreamList;
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer } from 'redux-form';
 import authReducer from './authReducer';
+import streamsReducer from './streamsReducer';
 
 export default combineReducers({
+	streams: streamsReducer,
 	auth: authReducer,
 	form: reducer,
 });
