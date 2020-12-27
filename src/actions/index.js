ab466380bf614dee9779c7fd6b37d5bf55f0c046
jolentino <johnm.tolentino@gmail.com>
2020-12-27 12:46:29 -0500

    Created actions and reducers for network requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,9 +15,40 @@ export const signOut = () => {
 
 export const createStream = (formValues) => {
 	return async (dispatch) => {
-		// Make a POST request to our endpoint with our values.
 		const { data } = await streams.post('/streams', formValues);
 
 		dispatch({ type: 'CREATE_STREAM', payload: data });
+	};
+};
+
+export const fetchStreams = () => {
+	return async (dispatch) => {
+		const { data } = await streams.get('/streams');
+
+		dispatch({ type: 'FETCH_STREAMS', payload: data });
+	};
+};
+
+export const fetchStream = (id) => {
+	return async (dispatch) => {
+		const { data } = await streams.get(`/streams/${id}`);
+
+		dispatch({ type: 'FETCH_STREAM', payload: data });
+	};
+};
+
+export const editStream = (id, formValues) => {
+	return async (dispatch) => {
+		const { data } = await streams.put(`/streams/${id}`, formValues);
+
+		dispatch({ type: 'EDIT_STREAM', payload: data });
+	};
+};
+
+export const deleteStream = (id) => {
+	return async (dispatch) => {
+		await streams.delete(`/streams/${id}`);
+
+		dispatch({ type: 'DELETE_STREAM', payload: id });
 	};
 };
